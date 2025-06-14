2bf2e2583f901a9641ddecf6dcdae483743c0132
AfafTabana <96386311+AfafTabana@users.noreply.github.com>
2025-06-14 09:02:33 +0300

    Hosted

--- APID02/Program.cs
+++ APID02/Program.cs
@@ -61,11 +61,12 @@ namespace APID02
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-                app.UseSwaggerUI(op => op.SwaggerEndpoint("/openapi/v1.json", "v1"));
-            }
+           // if (app.Environment.IsDevelopment())
+           // {
+               
+           // }
+            app.MapOpenApi();
+            app.UseSwaggerUI(op => op.SwaggerEndpoint("/openapi/v1.json", "v1"));
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
